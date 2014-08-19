be824ce0bd27fbe3c3a1b29aaaff2d186ba9d3ed
namreeb <github@namreeb.org>
2014-08-19 08:50:41 -1000

    - added option to inject into running process

--- hacksniff/main.cpp
+++ hacksniff/main.cpp
@@ -8,11 +8,13 @@
 #include <boost/exception/diagnostic_information.hpp>
 
 #include <hadesmem/injector.hpp>
+#include <hadesmem/process.hpp>
 
 int main(int argc, char *argv[])
 {
     std::wstring dll, program, logfile;
     std::string exportFunc;
+    int processId;
 
     try
     {
@@ -22,7 +24,8 @@ int main(int argc, char *argv[])
             ("logfile,l", boost::program_options::wvalue<std::wstring>(&logfile)->default_value(L"snifflog.txt", "snifflog.txt"), "log file")
             ("dll,d", boost::program_options::wvalue<std::wstring>(&dll)->default_value(L"monitor.dll", "monitor.dll"), "dll to inject into program")
             ("export,e", boost::program_options::value<std::string>(&exportFunc)->default_value("Load"), "export to call once dll is injected")
-            ("program,p", boost::program_options::wvalue<std::wstring>(&program)->required(), "program name");
+            ("program,p", boost::program_options::wvalue<std::wstring>(&program), "program name")
+            ("pid,i", boost::program_options::value<int>(&processId), "process id");
 
         boost::program_options::variables_map vm;
 
@@ -36,6 +39,13 @@ int main(int argc, char *argv[])
                 std::cout << desc << std::endl;
                 return EXIT_FAILURE;
             }
+
+            if (!vm.count("program") && !vm.count("pid"))
+            {
+                std::cerr << "ERROR: Must specify program or process ID" << std::endl << std::endl;
+                std::cerr << desc << std::endl;
+                return EXIT_FAILURE;
+            }
         }
         catch (boost::program_options::error const &e)
         {
@@ -44,12 +54,33 @@ int main(int argc, char *argv[])
             return EXIT_FAILURE;
         }
 
-        std::vector<std::wstring> createArgs;
+        if (vm.count("program"))
+        {
+            std::vector<std::wstring> createArgs;
 
-        const hadesmem::CreateAndInjectData injectData =
-            hadesmem::CreateAndInject(program, L"", std::begin(createArgs), std::end(createArgs), dll, exportFunc, hadesmem::InjectFlags::kPathResolution);
+            const hadesmem::CreateAndInjectData injectData =
+                hadesmem::CreateAndInject(program, L"", std::begin(createArgs), std::end(createArgs), dll, exportFunc, hadesmem::InjectFlags::kPathResolution);
 
-        std::wcout << L"Injected.  Process ID: " << injectData.GetProcess() << std::endl;
+            processId = injectData.GetProcess().GetId();
+        }
+        else
+        {
+            try
+            {
+                const hadesmem::Process process(processId);
+
+                auto dllHandle = hadesmem::InjectDll(process, dll, hadesmem::InjectFlags::kPathResolution);
+
+                hadesmem::CallExport(process, dllHandle, exportFunc);
+            }
+            catch (boost::program_options::error const &e)
+            {
+                std::cerr << "ERROR: " << e.what() << std::endl << std::endl;
+                return EXIT_FAILURE;
+            }
+        }
+
+        std::wcout << L"Injected.  Process ID: " << processId << std::endl;
     }
     catch (std::exception const &e)
     {
